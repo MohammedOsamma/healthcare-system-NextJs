6f17932709a5d0e2472035d80a6464d59bcc86a6
MohammedOsamma <mo856915i@gmail.com>
2025-05-31 16:17:35 +0200

    Fix defaultsValues Bug in RegisterForm & Create new-appointment Page

---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Logo from "@/public/assets/icons/logo-full.svg";
+import Appointment from "@/public/assets/images/appointment-img.png";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen min-h-screen   ">
+      <section className="container  remove-scrollbar my-auto">
+        <div className="sub-container max-w-[860px]">
+          <Image
+            height={1000}
+            width={1000}
+            src={Logo}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          {/* <PatientForm /> */}
+
+          <p className="justify-items-end text-[#76828D] xl:text-left">
+            ©2025 CarePulse
+          </p>
+        </div>
+      </section>
+      <Image
+        src={Appointment}
+        alt="appointment"
+        height={1000}
+        width={1000}
+        className="side-img max-w-[390px] rounded-l-2xl"
+      />
+    </div>
+  );
+}
