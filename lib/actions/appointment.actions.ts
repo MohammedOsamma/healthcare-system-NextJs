fc37fb653d34f340acad25259468ec5b79b54b17
MohammedOsamma <mo856915i@gmail.com>
2025-06-02 20:49:56 +0200

    Create createAppointment Function Server Action in appointment.actions.ts file

---
+++ lib/actions/appointment.actions.ts
@@ -0,0 +1,23 @@
+import { ID } from "node-appwrite";
+import {
+  databases,
+  NEXT_PUBLIC_APPOINTMENTS_COLLECTION_ID,
+  NEXT_PUBLIC_DATABASE_ID,
+} from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+export const createAppointment = async (
+  appointment: CreateAppointmentParams
+) => {
+  try {
+    const newAppointment = await databases.createDocument(
+      NEXT_PUBLIC_DATABASE_ID!,
+      NEXT_PUBLIC_APPOINTMENTS_COLLECTION_ID!,
+      ID.unique(),
+      appointment
+    );
+    return parseStringify(newAppointment);
+  } catch (error) {
+    console.error("Error creating appointment:", error);
+  }
+};
