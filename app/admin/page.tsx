5ee699d890f62584779de6ebdf882acbc04eb951
MohammedOsamma <mo856915i@gmail.com>
2025-06-03 18:30:29 +0200

    Add Structure with some Style to StatCard Component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import Logo from "@/public/assets/icons/logo-full.svg";
 import StatCard from "@/components/StatCard";
-
+import AppointmentIcon from "@/public/assets/icons/appointments.svg";
+import PendingIcon from "@/public/assets/icons/pending.svg";
+import CancelIcon from "@/public/assets/icons/cancelled.svg";
 const Admin = () => {
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-14">
@@ -21,12 +23,29 @@ const Admin = () => {
       <main className="admin-main">
         <section className="w-full space-y-4">
           <h1 className="header">Welcome 👋 </h1>
-          <p className="text-dark-700">
+          <p className="text-[#ABB8C4]">
             Start day with manageing new Appointment
           </p>
         </section>
         <section className="admin-stat">
-          <StatCard />
+          <StatCard
+            type="appointments"
+            count={5}
+            label="Schedule appointments"
+            icon={AppointmentIcon}
+          />
+          <StatCard
+            type="pending"
+            count={10}
+            label="Pending Appointments"
+            icon={PendingIcon}
+          />
+          <StatCard
+            type="cancelled"
+            count={2}
+            label="Cancelled Appointments"
+            icon={CancelIcon}
+          />
         </section>
       </main>
     </div>
